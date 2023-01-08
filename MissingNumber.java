a0aab7347e7edbe4d5c2bd133c856643a847361f
Dhayaneshwar <63561465+dhayanesh@users.noreply.github.com>
2023-01-08 03:25:23 -0500

    Leetcode "Missing Numbers" problem

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,7 +1,19 @@
-//Solution 1:
+//Solution 1 (0 ms):
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i = 0; i <= nums.length; i++) {
+            sum1 = sum1 + i;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            sum2 = sum2 + nums[i];
+        }        
+        return sum1 - sum2;
+    }
+}
 
-
-//Solution 2:
+//Solution 2 (6 ms):
 class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
