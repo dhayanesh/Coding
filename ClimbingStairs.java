2c9a0cf11bffeb273ce1d224800bc4fa22fef406
Dhayaneshwar <63561465+dhayanesh@users.noreply.github.com>
2023-01-10 17:05:37 -0500

    Leetcode "Climbing Stairs" problem

---
+++ ClimbingStairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    HashMap<Integer, Integer> dp = new HashMap<>();
+    public int climbStairs(int n) {
+        if(n == 0)
+            return 1;
+        if(n < 0)
+            return 0;
+        if(dp.containsKey(n))
+            return dp.get(n);
+        int count = climbStairs(n-1) + climbStairs(n-2);
+        dp.put(n, count);
+        return count;
+    }
+}
