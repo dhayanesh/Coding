9c1725e7eeec96874134b37652ddc862ccfeb781
Dhayaneshwar <63561465+dhayanesh@users.noreply.github.com>
2023-01-13 00:23:55 -0500

    Leetcode "Sqrt(x)" problem

---
+++ Sqrt.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mySqrt(self, x):
+        left = 0
+        right = x
+        while left <= right:
+            mid = (left + right) / 2
+            if(mid*mid == x):
+                return mid
+            elif(mid*mid < x):
+                left = mid + 1
+                res = mid
+            else:
+                right = mid - 1
+        return res
