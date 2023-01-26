ae2b924d8a799861aa9f3b7f3c6b563102cf8794
Dhayaneshwar <63561465+dhayanesh@users.noreply.github.com>
2023-01-26 01:48:15 -0500

    HackerRank "Making Anagrams" problem

---
+++ makingAnagrams.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(String s1, String s2) {
+    // Write your code here
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        int i = 0, j = 0, count = 0;
+        while(i < s1.length() && j < s2.length()) {
+            if(ch1[i] < ch2[j]) {
+                i++;
+            }
+            else if(ch1[i] > ch2[j]) {
+                j++;
+            }   
+            else if(ch1[i] == ch2[j]) {
+                i++;
+                j++;
+                count++;
+            }
+        }             
+        return s1.length() + s2.length() - 2 * count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
